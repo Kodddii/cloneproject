310d6056b5d7b69eb235765a48e6c61e96b771cd
Kodddii <justic521@gmail.com>
2022-04-16 23:37:05 +0900

    itempage, dbsetup

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 //express 모듈 불러오기
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connect = require("./schemas");
-require("dotenv").config();
+const bodyParser = require("body-parser")
 const app = express();
 const port = 3000;
 
 //라우터
 const loginRouter = require("./routers/logins");
-
+const itemRouter = require("./routers/itempage")
 connect();
 
 //접속로그 확인
@@ -22,9 +23,10 @@ app.use(cors());
 app.use(express.json());
 app.use(requestMiddleWare);
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 //라우터 연결
-app.use("/", [loginRouter]);
+app.use("/", [loginRouter,itemRouter]);
 
 app.listen(port, () => {
   console.log(port, "번으로 서버가 켜졌어요!");
