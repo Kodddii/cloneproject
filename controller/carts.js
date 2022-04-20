202cb75c7cf39e4de745fccf92b99e3256a6f7b7
Kodddii <justic521@gmail.com>
2022-04-20 15:44:27 +0900

    Update carts.js

--- controller/carts.js
+++ controller/carts.js
@@ -1,10 +1,12 @@
+const res = require("express/lib/response");
 const Cart = require("../schemas/cart");
 const User = require("../schemas/user");
  
 //장바구니에 상품 담기
 const addCart = async (req, res) => {
-  const { user } = res.locals;
-  const { itemId, itemName, itemAmount, itemPrice, userAddress, itemCategory, itemImg,userId } = req.body;
+  // const { user } = res.locals;
+  // const userId = user[0].userId
+  const { itemId, itemName, itemAmount, itemPrice, userAddress, itemCategory, itemImg, userId} = req.body;
   const userCartData = {
     itemId: itemId,
     itemName: itemName,
@@ -40,17 +42,29 @@ const editCart = async(req,res)=>{
   const { user } = res.locals
   const userId = user[0].userId
   const {itemId, itemAmount, itemPrice} = req.body;
-await User.updateOne({userId:userId,"userCart.itemId": itemId},
-{$set:{"userCart.$.itemAmount": itemAmount,"userCart.$.itemPrice":itemPrice},});
-res.status(201).send()}
+  try {
+    await User.updateOne({userId:userId,"userCart.itemId": itemId},
+    {$set:{"userCart.$.itemAmount": itemAmount,"userCart.$.itemPrice":itemPrice},});
+  return res.status(201).send()}
+    
+   catch (error) {
+    res.status(400)
+  }
+}
+  
+
 
 
 //장바구니 삭제
 const deleteCart = async (req, res) => {
   const { userId, itemId } = req.body;
-  await User.updateOne({ userId }, { $pull: { userCart: { itemId } } });
-  res.json({ result: "deleteSuccess" });
+  for (x of itemId){
+    console.log(x)
+  await User.updateOne({ userId }, { $pull: { userCart:{ itemId:x } }});
+  
 };
+res.json({ result: "deleteSuccess" });
+}
 
 
 module.exports = {addCart,readCart,editCart, deleteCart}  ;
